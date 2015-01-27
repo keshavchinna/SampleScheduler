c524c3e0783efe028f4a7770e5d13e6acf610dc6
Keshav Yamalapati <keshav@eggheadcreative.com>
2015-01-27 19:37:49 +0530

    added mongo replated files and modal

--- src/main/java/com/ehc/service/SchedulerServiceImpl.java
+++ src/main/java/com/ehc/service/SchedulerServiceImpl.java
@@ -11,6 +11,7 @@ public class SchedulerServiceImpl implements SchedulerServiceI {
 
   public void executeScheduleTask() {
     getSchedulerTask().spendBasedLoyalty();
+
   }
 
 
--- src/main/java/com/ehc/starter/Application.java
+++ src/main/java/com/ehc/starter/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @ComponentScan
 //@EnableAutoConfiguration
 //@EnableMongoRepositories
-//@ImportResource("classpath:SpringContext.xml")
+//@ImportResource("classpath:applicationContext.xml")
 public class Application {
   public static void main(String[] args) {
     new ClassPathXmlApplicationContext("applicationContext.xml");
